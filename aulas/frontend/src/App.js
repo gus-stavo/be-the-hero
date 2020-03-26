4341bb8a70736b3a7f80aed2dcfb016d658a9344
gus-stavo <gustavomlstefano@gmail.com>
2020-03-26 00:41:22 -0300

    Front-end da aplicação

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+/**
+ * Componente: no react é uma função que retorna html
+ * JSx (JavaScript XML): Xml é a sintaxe do Html, jsx é o html que esta escrito no arquivo javascript
+ * ********Propriedades: no react, são como os atributos do html,**********
+ * Componentes sempre tem que começar com letra maisucula, exemplo: Header.js
+ * Estado: Informação que será mantida pelo componente, exemplo input ou lista de usuarios
+ * Estado retorna um Array [valor, funcaoDeAtualizacao]
+ * 
+ * react icons - icones 
+ * react router dom - rotas
+ * axios - conexao com banco de dados
+ */
